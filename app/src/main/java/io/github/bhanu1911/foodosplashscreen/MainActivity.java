1cc09f0226bb77651b31d93859cf8f32a45e18c6
Bhanu Bhandari <bhanubhandari99@gmail.com>
2017-12-16 01:37:31 +0530

    Pushed splash screen to git

---
+++ app/src/main/java/io/github/bhanu1911/foodosplashscreen/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.bhanu1911.foodosplashscreen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
